cf8dd53742f7a0adc51386ef288084b51c6f2bf8
Dario Faggioli <dfaggioli@suse.com>
2022-08-29 17:38:59 +0200

    Support more complex testplans

---
+++ setups/repo_rpms_setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -x
+
+echo $@
+
+exit 0
--- testplan_step.sh
+++ testplan_step.sh
@@ -6,16 +6,39 @@ export MMCI_HOSTDIR="${MMCI_DIR}/$(hostname -s)"
 
 . "${MMCI_DIR}/common.sh"
 
+# TODO: parametrize this
+RESULTS="${DIR}/mmci-results/"
+
 log "STARTING $(realpath $0)"
 
-LASTLINE=$(cat ${MMCI_HOSTDIR}/testplan.step 2> /dev/null || echo "START")
+L=0
+LASTLINE=$(cat ${MMCI_HOSTDIR}/testplan.step 2> /dev/null || echo "0")
 while read -r -u 3 LINE; do
-	[ "$LINE" != "$LASTLINE" ] && continue
+	L=$(($L + 1))
+	[ "$LINE" == "START" ] && continue
+	[ "$LINE" ~= "^#.*" ] && continue
+	[ $L -le $LASTLINE ] && continue
 	[ "$LINE" == "END" ] && break
+	if [ "$LINE" ~= "^TEST.*" ]; then
+		TESTNAME=$(echo "$LINE" | cut -f2 -d' ')
+		#read -r -u 3 LINE
+	fi
 	read -r -u 3 LINE
-	log "RUNNING $LINE"
-	sleep 180 # FIXME: ~ ./$LINE
-	echo "$LINE" > ${MMCI_HOSTDIR}/testplan.step
+	L=$(($L + 1))
+	if [ -f "${MMCI_DIR}/${LINE}" ]; then
+		log "WARNING: script ${MMCI_DIR}/${LINE} is missing. Trying to continue..."
+	else
+		log "RUNNING $LINE"
+		eval ${MMCI_DIR}/${LINE} $TESTNAME
+		if [ $? -ne 0 ]; then
+			# The script failed. Either there's no need to run this
+			# test, or something did not work. In any case, let's
+			# jump to the next one
+			LL=$(($L + 1))
+			L=$(cat -n Zhaman/testplan |tail -n +${LL}|grep -B1 -m1 setups|head -1|cut -f1|xargs)
+		fi
+	fi
+	echo $L > ${MMCI_HOSTDIR}/testplan.step
 	break
 done 3< ${MMCI_HOSTDIR}/testplan
 
