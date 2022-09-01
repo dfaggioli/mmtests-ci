7acc355ca52ec149a63a9ae64d165dc6925ff996
Dario Faggioli <dfaggioli@suse.com>
2022-09-01 18:52:33 +0200

    Fix typo when installing packages

--- setups/official_rpms_setup.sh
+++ setups/official_rpms_setup.sh
@@ -8,7 +8,7 @@ update_OS
 # FIXME: Move this into its own setups/install_kvm_rpms.sh base|all
 #KVM_INSTALL_BASE="qemu-x86 tftp libvirt-daemon-qemu virt-install libvirt-client libvirt-daemon-config-network tigervnc virt-manager vm-install"
 KVM_INSTALL_ALL_PATTERNS="kvm_server kvm_tools"
-$MMCI_PACKAGES_INSTALL -t pattern --recommends $KVM_INSTALL_PATTERNS
+$MMCI_PACKAGES_INSTALL -t pattern --recommends $KVM_INSTALL_ALL_PATTERNS
 
 log "DONE official_rpms_setup.sh"
 
