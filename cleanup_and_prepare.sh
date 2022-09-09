87c4d88a86ca0f7ceeeb0d3a745014e35a14d404
Dario Faggioli <dfaggioli@suse.com>
2022-09-09 12:39:05 +0200

    Cleanup

--- cleanup_and_prepare.sh
+++ cleanup_and_prepare.sh
@@ -4,14 +4,16 @@ systemctl disable --now mmci
 rm /etc/systemd/system/mmci.service
 systemctl daemon-reload
 
+# FIXME: All these names should come from config...
 pushd /root
 rm -f mmci_pause mmci_term mmci_curr_step.sh mmci_next_step.sh mmci_step.sh mmci.service
 rm -f install_and_start.sh
 rm -rf mmtests-ci
 rm -rf mmtests
-rm -rf mmci_logs
-echo "For cleaning up results, do:"
-echo "rm -rf mmci_results"
+rm -rf mmci-logs
+echo "Opportunities of further cleanups:"
+echo " rm -rf mmci-results"
+echo " rm -rf qemu"
 popd
 
 wget https://raw.githubusercontent.com/dfaggioli/mmtests-ci/main/install_and_start.sh
